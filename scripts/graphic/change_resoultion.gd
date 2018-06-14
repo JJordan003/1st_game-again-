0f7291ab720ec2f76e1b34ffb90f0e3c32725c64
JJordan003 <34778792+JJordan003@users.noreply.github.com>
2018-06-14 16:43:51 +0200

    v0.0.0g
    +Dodano zmianę rozdzielczości

--- scripts/graphic/change_resoultion.gd
+++ scripts/graphic/change_resoultion.gd
@@ -1,9 +1,14 @@
 extends Button
-
+var r1920 = Vector2(1920,1080)
+var s1920 = Vector2(1,1)
 func _ready():
-
 	pass
 func _pressed():
-	get_node("/root/control/container").scale.x = 1
-	get_node("/root/control/container").scale.y = 1
+	get_node("/root/control/container").set_scale(s1920)
+	#OS.set_window_fullscreen(true) włącza fullscreen
+	#OS.set_borderless_window(value) usuwa border
+	if OS.is_window_fullscreen(true):
+		get_node("/root").set_size(r1920)
+	else:
+		OS.set_window_size(r1920)
 	pass
