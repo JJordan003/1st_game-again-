157242f98fb503a3ce8e6eb6053001bf3644ac70
JJordan003 <34778792+JJordan003@users.noreply.github.com>
2018-06-14 18:07:05 +0200

    v0.0.0k

--- scripts/graphic/set_fullscreen.gd
+++ scripts/graphic/set_fullscreen.gd
@@ -1,10 +1,7 @@
 extends Button
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 func _pressed():
-	print(OS.get_window_size())
 	if OS.is_window_fullscreen():
 		OS.set_window_minimized(true)
 	else:
---
+++ scripts/test/reso.gd
@@ -0,0 +1,5 @@
+extends Button
+func _ready():
+	pass
+func _pressed():
+	print(OS.get_real_window_size())
