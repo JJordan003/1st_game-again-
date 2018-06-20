6eadfb5f30a9cb2ab83689a0ac36ac6e159dc5fc
pikol93 <pikol93@users.noreply.github.com>
2018-06-20 19:23:07 +0200

    v0.0.4
    +black_box.tscn - scena uzywana do tworzenia nowych elementow na level.tscn (placeholder, sluzy do testow)
    +dodano mozliwosc tworzenia nowych elementow na level.tscn (obiekty z tekstura i kolizja)
    *usprawniono skrypt sluzacy do poruszania sie
    *mniej wazne zmiany w level.tscn (exportowane zmienne)
    -usunieto nieuzywany skrypt
    
    /*
    zastanawiam sie czy nie zabrac tego projektu i nie kontynuowac go samemu ;/
    w sumie juz wszystko sam zrobilem
    */

--- scripts/engine/level_restart.gd
+++ scripts/engine/level_restart.gd
@@ -7,7 +7,8 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("game_spawn"):
 		print("SPAWNED at: ",get_global_mouse_position())
-		get_node("level/StaticBody2D1").duplicate().position = get_global_mouse_position()
+		get_node("level").add_child(load("res://scenes/entities/black_box.tscn").instance())
+		get_node("level").get_child(get_node("level").get_child_count()-1).position = get_local_mouse_position()
 	pass
 
 func _ready():
--- scripts/player/player_movement.gd
+++ scripts/player/player_movement.gd
@@ -6,43 +6,42 @@ export (float) var AIR_ACCELERATION
 export (int) var MAX_SPEED
 export (float) var SLOW_DOWN_SPEED
 export (int) var JUMP_HEIGHT
+export (int) var MAX_ACCELERATION
+export (float) var ACCELERATION_MULTIPLIER 
 var debug = false
 var velocity = Vector2()
-var acceleration = 0
+var acceleration = 0.1
 var movespeed = Vector2()
 
 func is_colliding(area):
-	if get_node(area).get_overlapping_bodies().size() != 0:
-		if get_node(area).get_overlapping_bodies()[0] != get_node("."):
+	for body in get_node(area).get_overlapping_bodies().size():
+		if get_node(area).get_overlapping_bodies()[body-1] != get_node("."):
 			return true
 			pass
-		else:
-			return false
 		pass
-	if get_node(area).get_overlapping_bodies().size() == 0:
-		return false
-		pass
+	return false
 	pass
 	
 func _process(delta):
-	#
-	#MOVEMENT:
-	#
-	if Input.is_action_pressed("ui_left"):
-		acceleration -= 1
-		pass #increasing acceleration with pressing binded key
-	if Input.is_action_pressed("ui_right"):
-		acceleration += 1
-		pass #increasing acceleration with pressing binded key
 	
-	if acceleration > 0:
-		acceleration -= SLOW_DOWN_SPEED/100
-		pass #slowing down when acceleration is positive
-	if acceleration < 0:
-		acceleration += SLOW_DOWN_SPEED/100
-		pass #slowing down when acceleration is negative
+	if is_colliding("collisions/collision_ground"):
+		if Input.is_action_pressed("ui_left"):
+			acceleration -= ACCELERATION_MULTIPLIER
+			pass #increasing acceleration with pressing binded key
+		if Input.is_action_pressed("ui_right"):
+			acceleration += ACCELERATION_MULTIPLIER
+			pass #increasing acceleration with pressing binded key
+		pass #is responsible for accelerating while on the ground
+	else:
+		if Input.is_action_pressed("ui_left"):
+			acceleration -= ACCELERATION_MULTIPLIER * AIR_ACCELERATION
+			pass #increasing acceleration with pressing binded key
+		if Input.is_action_pressed("ui_right"):
+			acceleration += ACCELERATION_MULTIPLIER * AIR_ACCELERATION
+			pass #increasing acceleration with pressing binded key
+		pass #is responsible for accelerating while in the air
 	
-	if Input.is_action_pressed("player_jump") and is_colliding("collisions/collision_ground") == true:
+	if Input.is_action_just_pressed("player_jump") and is_colliding("collisions/collision_ground"):
 		velocity.y = -JUMP_HEIGHT
 		pass #jumping
 	
@@ -50,7 +49,7 @@ func _process(delta):
 		velocity.y += GRAVITY * delta
 		pass #gravity / this makes player fall
 	
-	if !Input.is_action_pressed("player_jump") and is_colliding("collisions/collision_ground") == true:
+	if !Input.is_action_pressed("player_jump") and is_colliding("collisions/collision_ground"):
 		velocity.y = 0
 		pass #stopping gravity when on surface
 	
@@ -64,18 +63,38 @@ func _process(delta):
 	elif is_colliding("collisions/collision_right") and acceleration > 0:
 		acceleration = 0
 		pass #detects if there is a collision on right side of player and reduces acceleration preventing from being stuck in a wall
-	velocity.x = velocity.x + (acceleration * SPEED)
+	velocity.x += acceleration * SPEED
 	
+	if acceleration > MAX_ACCELERATION:
+		acceleration = MAX_ACCELERATION
+		pass #limiting acceleration to not have crazy speeds
+	if acceleration < -MAX_ACCELERATION:
+		acceleration = -MAX_ACCELERATION
+		pass #limiting acceleration to not have crazy speeds
 	if velocity.x > MAX_SPEED:
 		velocity.x = MAX_SPEED
 		pass #limiting speed, so that the player wont be able to go crazy high speeds
 	if velocity.x < -MAX_SPEED:
 		velocity.x = -MAX_SPEED
 		pass #limiting speed, so that the player wont be able to go crazy high speeds
-		
+	
+	if is_colliding("collisions/collision_ground"):
+		if !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right"):
+			acceleration = 0
+			pass
+		pass #is responsible for slowing down player when not accelerating
+	if debug == true:
+		print("acceleration :",acceleration,", velocity.x: ",velocity.x,", velocity.y: ",velocity.y, is_colliding("collisions/collision_ground"))
+		pass #various purposes
 	movespeed.x = delta * velocity.x
 	movespeed.y = delta * velocity.y
 	move_and_collide(movespeed) #putting all the above variables into use 
 	velocity.x = 0
+	
+	if Input.is_action_just_pressed("game_debug"):
+		if debug == false:
+			debug = true
+		else:
+			debug == false
 	pass
 	#dislaimer: i don't remember what and why i wrote all this, if it contains some bugs it'll be an ass to fix so please don't report any thanks
