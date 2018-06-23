2ba90939a3e262d65b562f1b4476be8e2cbf1a53
pikol93 <pikol93@users.noreply.github.com>
2018-06-23 18:06:50 +0200

    v0.0.4d
    +white_pixel.png
    +kilka nowych wpisow w Input Map
    +teraz mozna usuwac stworzone elementy (bardzo zasobozerne)
    *kilka mniejszych zmian w skryptach

--- scripts/level/spawn.gd
+++ scripts/level/spawn.gd
@@ -4,42 +4,70 @@ var history = []
 
 func ent(ent): #spawns entity at mouse position
 	#ent = lokalizacja sceny na dysku
-	var operating_node = get_node(level_tree).get_child(0)
+	var operating_node = get_node(level_tree)
 	var spawn_position = Vector2()
 	var mouse_position = Vector2()
 	mouse_position.x = operating_node.get_local_mouse_position().x+32
 	mouse_position.y = operating_node.get_local_mouse_position().y+32
-
-	operating_node.add_child(load(ent).instance())
-
+	
 	while spawn_position.x < mouse_position.x:
 		spawn_position.x += 64
 		pass
 	while spawn_position.x > mouse_position.x:
 		spawn_position.x -= 64
 		pass
-
 	while spawn_position.y < mouse_position.y:
 		spawn_position.y += 64
 		pass
 	while spawn_position.y > mouse_position.y:
 		spawn_position.y -= 64
 		pass
-
-	print("\n***\nMOUSE POSITION       :",mouse_position)
-	print("ATTEMPTING TO SPAWN ENTITY AT: ",spawn_position)
+	
 	if exists(spawn_position): #if something exists at given position
-		print("SOMETHING EXISTS THERE ALREADY!")
 		pass
 	else: #if nothing exists at given position
-		print("SPAWNED ",ent," AT: ", spawn_position)
+		operating_node.add_child(load(ent).instance())
 		operating_node.get_child(operating_node.get_child_count()-1).position = spawn_position
 		history.push_front(spawn_position)
 		pass
 	pass
 
+func despawn():
+	var operating_node = get_node(level_tree)
+	var despawn_position = Vector2()
+	var mouse_position = Vector2()
+	var debug_counter = 0
+	mouse_position.x = operating_node.get_local_mouse_position().x+32
+	mouse_position.y = operating_node.get_local_mouse_position().y+32
+	
+	while despawn_position.x < mouse_position.x:
+		despawn_position.x += 64
+		pass
+	while despawn_position.x > mouse_position.x:
+		despawn_position.x -= 64
+		pass
+
+	while despawn_position.y < mouse_position.y:
+		despawn_position.y += 64
+		pass
+	while despawn_position.y > mouse_position.y:
+		despawn_position.y -= 64
+		pass
+	
+	if exists(despawn_position): #if something exists at given position
+		for child in operating_node.get_children():
+			if child.position == despawn_position:
+				child.queue_free()
+				return
+				pass
+			pass
+		var array_counter = 0
+		history.remove(history.find_last(despawn_position))
+		pass
+	pass #very inefficient code when operating on large numbers of entities 
+
 func exists(pos): #checks if entity exists at given position
-	print(pos,"\n",history)
+#	print(pos,"\n",history)
 	for index in history:
 		if pos == index:
 			return true
--- scripts/scene_editor.gd
+++ scripts/scene_editor.gd
@@ -1,29 +1,43 @@
 extends Container
 
 onready var camera = get_node("camera")
+var entity = "res://scenes/entities/black_box.tscn"
 
 func _ready():
+	spawn.level_tree = get_child(0).get_path()
 	set_process(true)
 	pass
 
-func _process():
-	print("x")
-	if Input.is_action_pressed("ui_left"):
-		camera.position.x -= 1
+func _process(delta):
+	#ENGINE:
+	#MOVEMENT AND ZOOM
+	if Input.is_action_pressed("game_left"):
+		camera.position.x -= 5 * camera.zoom.x
 		pass
-	if Input.is_action_pressed("ui_right"):
-		camera.position.x += 1
+	if Input.is_action_pressed("game_right"):
+		camera.position.x += 5 * camera.zoom.x
 		pass
-	if Input.is_action_pressed("ui_up"):
-		camera.position.y -= 1
+	if Input.is_action_pressed("game_up"):
+		camera.position.y -= 5 * camera.zoom.y
 		pass
-	if Input.is_action_pressed("ui_down"):
-		camera.position.y += 1
+	if Input.is_action_pressed("game_down"):
+		camera.position.y += 5 * camera.zoom.y
 		pass
-	
-	if Input.is_action_just_pressed("scroll_up"):
+	if Input.is_action_just_pressed("editor_zoomout"):
 		camera.zoom *= 1.5
 		pass
-	if Input.is_action_just_pressed("scroll_down"):
+	if Input.is_action_just_pressed("editor_zoomin"):
 		camera.zoom /= 1.5
 		pass
+	#ENTITY PLACEMENT
+	if Input.is_action_pressed("editor_spawn"):
+		spawn.ent(entity)
+		pass
+	if Input.is_action_pressed("editor_despawn"):
+		spawn.despawn()
+		pass
+	pass
+	
+	#GUI:
+	#MOUSE_POSITION
+	get_node("camera/gui_info/mouse_position").bbcode_text = str(get_global_mouse_position())
