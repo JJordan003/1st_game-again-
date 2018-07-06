ca6e08bafe58d730a51faf798f5f55c069cc10a6
pikol93 <pikol93@users.noreply.github.com>
2018-07-06 17:10:28 +0200

    v0.0.5a

--- scripts/gui/editor/material_white_box.gd
+++ scripts/gui/editor/material_white_box.gd
@@ -1,5 +1,5 @@
 extends Button
 
 func _pressed():
-	get_node("/root/control/container/viewport/scene_editor").entity = "res://scenes/entities/white_box.tscn"
+	get_node("/root/control/container/scene_editor").entity = "res://scenes/entities/white_box.tscn"
 	pass
